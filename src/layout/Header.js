8e0bed4ad7c5f0c0914ea65cf4566b011c55e479
rajichebrolu <raji.chebrolu@gmail.com>
2024-08-20 12:54:53 +1200

    Adding header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <header className="header">
+      <div className="logo">La Monijor Bakery</div>
+      <nav className="nav">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+        <a href="#products">Products</a>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
